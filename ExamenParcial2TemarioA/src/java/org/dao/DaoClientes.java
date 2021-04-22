b2c9995584fefcc86ee7f73f5b350a72b5aa1edb
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-04-22 07:33:12 -0600

    Update DaoClientes.java

--- ExamenParcial2TemarioA/src/java/org/dao/DaoClientes.java
+++ ExamenParcial2TemarioA/src/java/org/dao/DaoClientes.java
@@ -1,14 +1,46 @@
 package org.dao;
-
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.config.Conexion;
 import org.interfaces.CrudClientes;
 import org.modelos.Clientes;
 
 public class DaoClientes implements CrudClientes{
+    
+    Clientes clientes = new Clientes();
+    String sql = "";
+    Conexion con = new Conexion();
+    ResultSet rs = null;
+    boolean resp = false;
 
     @Override
     public List listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Clientes> lstCliente = new ArrayList<>();
+        try{
+            sql = "SELECT * FROM CLIENTE";
+            con.open();
+            rs = con.executeQuery(sql);
+            while(rs.next()){
+                Clientes cliente = new Clientes();
+                cliente.setIdCliente(rs.getInt("ID_CLIENTE"));
+                cliente.setNombre(rs.getString("NOMBRE"));
+                cliente.setApellido(rs.getString("APELLIDO"));
+                cliente.setNit(rs.getString("NIT"));
+                cliente.setTelefono(rs.getString("TELEFONO"));
+                cliente.setDireccion(rs.getString("DIRECCION"));
+                lstCliente.add(cliente);
+            }
+            rs.close();
+            con.close();
+        } catch(ClassNotFoundException ex){
+            Logger.getLogger(DaoClientes.class.getName()).log(Level.SEVERE, null, ex);
+        } catch(Exception ex){
+            Logger.getLogger(DaoClientes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lstCliente;
     }
 
     @Override
