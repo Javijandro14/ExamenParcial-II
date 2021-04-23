940eacd606cd2e88b907c0ebae33aead9ec67b8e
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-04-23 09:29:54 -0600

    buscar municipio x depto

--- ExamenParcial2TemarioA/src/java/org/servicios/ServicioPedido.java
+++ ExamenParcial2TemarioA/src/java/org/servicios/ServicioPedido.java
@@ -11,9 +11,8 @@ import org.modelos.Pedido;
 public class ServicioPedido {
 
     DaoPedido daoPedido = new DaoPedido();
-    @WebMethod(operationName = "getPedido")
-    public List<Pedido> getPedido(@WebParam(name = "idPedido") int idPedido) {
-        List<Pedido> lstPedido = daoPedido.listar(idPedido);
-        return lstPedido;
+     @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
     }
 }
